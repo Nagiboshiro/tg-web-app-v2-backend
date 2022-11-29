6664a32e47244df6a7459c065657f959a0a85572
Nagiboshiro <vain335@gmail.com>
2022-11-29 16:33:54 +0300

    init

--- index.js
+++ index.js
@@ -60,29 +60,17 @@ const PORT = 8000;
 app.post('/data', async (req, res) => {
     const {queryId, products, totalPrice} = req.body;
 
-    try {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Успешная покупка',
-            input_message_content: {
-                message_text: 'Вы приобрели товар на сумму' + totalPrice
-            }
-        })
+    await bot.answerWebAppQuery(queryId, {
+        type: 'article',
+        id: queryId,
+        title: 'Успешная покупка',
+        input_message_content: {
+            message_text: 'Вы приобрели товар на сумму' + totalPrice
+        }
+    })
 
-        return res.status(200).json({});
-    } catch (e) {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Не удалость приобрести товар',
-            input_message_content: {
-                message_text: 'Не удалость приобрести товар'
-            }
-        })
+    return res.status(200).json({});
 
-        return res.status(500).json({});
-    }
 })
 
 app.listen(PORT, () => console.log('server start on port ' + PORT))
