47718efbd2e5754b6def93003420ea85b76fbfd5
Nagiboshiro <vain335@gmail.com>
2022-11-29 11:23:31 +0300

    init

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 
 const token = '5490204105:AAE5vE-WFchaKaXPWhGWDOYeL1E74koedQw'
-const webAppUrl = 'http://77.91.73.244:3000'
+const webAppUrl = 'https://superlative-gelato-313cf4.netlify.app/'
 
 const bot = new TelegramBot(token, {polling: true});
 const app = express()
